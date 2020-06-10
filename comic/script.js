a07b0b695b5fa5a865da52eaf34da064f5a0649c
Nurgazy Seidaliev <nks369@nyu.edu>
2020-06-10 16:35:37 +0400

    pages for another POV

---
+++ comic/script.js
@@ -0,0 +1,19 @@
+let current = "home"
+
+
+
+let mainelem = document.getElementById("main")
+let character = document.getElementById("character")
+let choice = document.getElementById("choice")
+
+main.style.display = "block"
+character.style.display = "none"
+choice.style.display = "none"
+
+main.style.display = "none" //this is hidden
+character.style.display = "block" //this is visible
+
+//......................block
+
+let image_choice = document.getElementById("image-choice")
+image_choice.src = "panel2.png"
